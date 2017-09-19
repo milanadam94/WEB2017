21a99a0588bf7c45e56cc906783c8ce28afd7504
milanadam94 <milan.adam94@gmail.com>
2017-09-19 23:52:23 +0200

    Pretrage na pocetnoj stranici.

--- Podforum/WebContent/app/controller/homeController.js
+++ Podforum/WebContent/app/controller/homeController.js
@@ -10,15 +10,23 @@ app.controller('homeController', ['$scope', '$location', '$cookies', 'subforumFa
 	$scope.viewSubforum = true;
 	$scope.subforumDetails = false;
 	
+	$scope.searchSub = { "description" : null, "name" : null, "responsibleModerator" : null }
+	$scope.searchTheme = { "tag" : null, "content" : null, "author" : null }
+	
+	$scope.searchThemes = [];
+	$scope.searchSubforums = [];
 	
 	subforumFactory.getSubforums().success(function(data){
 		$scope.subforums = data;
+		$scope.searchSubforums = data;
 	});
 	$scope.details = function(subforum){
 		$scope.subforumDetails = true;
+		$scope.themeDetails = false;
 		$scope.subforumDetail = subforum;
 		themeFactory.getThemes(subforum.name).success(function(data){
 			$scope.themes = data;
+			$scope.searchThemes = data;
 		});
 	}
 	$scope.openTheme = function(theme){
@@ -26,6 +34,38 @@ app.controller('homeController', ['$scope', '$location', '$cookies', 'subforumFa
 		$scope.themeDetail = theme;
 		commentFactory.getComments(theme.name).success(function(data){
 			$scope.comments = data;
+		});
+	}
+	$scope.searchSubforum = function(){
+		if(!$scope.searchSub.name)
+			$scope.searchSub.name = null;
+		if(!$scope.searchSub.description)
+			$scope.searchSub.description = null;
+		if(!$scope.searchSub.responsibleModerator)
+			$scope.searchSub.responsibleModerator = null;
+		
+		subforumFactory.search($scope.searchSub.name, $scope.searchSub.description, $scope.searchSub.responsibleModerator).success(function(data){
+			if(($scope.searchSub.name == null) && ($scope.searchSub.description == null) && ($scope.searchSub.responsibleModerator == null)){
+				$scope.subforums = $scope.searchSubforums;
+			}else{
+				$scope.subforums = data;
+			}
+		});
+	}
+	$scope.searchTheme = function(){
+		if(!$scope.searchTheme.tag)
+			$scope.searchTheme.tag = null;
+		if(!$scope.searchTheme.content)
+			$scope.searchTheme.content = null;
+		if(!$scope.searchTheme.author)
+			$scope.searchTheme.author = null;
+		
+		themeFactory.search($scope.searchTheme.tag, $scope.searchTheme.content, $scope.searchTheme.author).success(function(data){
+			if(($scope.searchTheme.tag == null) && ($scope.searchTheme.content == null) && ($scope.searchTheme.author == null)){
+				$scope.themes = $scope.searchThemes;
+			}else{
+				$scope.themes = data;
+			}
 		});
 	}
 	$scope.login = function(){
